8905ad045ac43fd9f7bcd57875087b0dce483298
36lean <mot.wu@36lean.com>
2013-08-31 18:18:16 +0800

    joomla-lean

---
+++ cache/template/yoo_lava.php
@@ -0,0 +1,1 @@
+{"tmpl_check":"474473a0c9e5c5d939b39b0d6c562192","tmpl_data":"","warp_check":"b0a87fa876e1f61aa8d627d99e5c1160","warp_data":"{\"status\":\"update-available\",\"message\":\"New version available! <a href=\\\"http:\\\/\\\/www.yootheme.com\\\" target=\\\"_blank\\\">Download Warp Theme Framework 7.1.6<\\\/a>\"}"}
